db3eb2cc3d8f54c1cf4ac92b8d86996928393356
TJ <tahmeenaowais@yahoo.com>
2020-08-21 07:27:03 -0400

    readme

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-{
-	"name": "Tahmeena",
-	"stack": [
-		"HTML"
+## Introduction{
+	"Project 1": "Binance",
+	"# About the Project": "Financila Literacy",
+	"# User Story": "to educate the user about stocks",
+	"Go live": "https://github.com/tjaved12/homeworknode#readme",
+	"Demo": "![image](https://user-images.githubusercontent.com/66760710/90885098-f3916d00-e37e-11ea-973c-293472b40c8a.png)",
+	"Tech Stack": [
+		"HTML",
+		"CSS",
+		"JavaScript",
+		"MySQL"
 	],
-	"contact": "phone"
+	"Contact": "email"
 }
--- index.js
+++ index.js
@@ -3,13 +3,33 @@ var fs = require("fs");
 
 inquirer.prompt([{
         type: "input",
-        name: "name",
-        message: "What is your name?"
+        name: "Project 1",
+        message: "What is your Project name?"
+    },
+    {
+        type: "editor",
+        name: "# About the Project",
+        message: "What is your Project about, write in 3 sentences?"
+    },
+    {
+        type: "editor",
+        name: "# User Story",
+        message: "What is your user story, write in 2 sentences?"
+    },
+    {
+        type: "link",
+        name: "Go live",
+        message: "What is your Project URL?"
+    },
+    {
+        type: "image",
+        name: "Demo",
+        message: "Paste an image?"
     },
     {
         type: "checkbox",
-        message: "What languages do you know?",
-        name: "stack",
+        message: "What languageswere used for the Project?",
+        name: "Tech Stack",
         choices: [
             "HTML",
             "CSS",
@@ -20,7 +40,7 @@ inquirer.prompt([{
     {
         type: "list",
         message: "What is your preferred method of communication?",
-        name: "contact",
+        name: "Contact",
         choices: [
             "email",
             "phone",
@@ -29,9 +49,8 @@ inquirer.prompt([{
     }
 ]).then(function (data) {
 
-    var filename = data.name.toLowerCase().split(' ').join('') + ".json";
-
-    fs.writeFile("README.md", JSON.stringify(data, null, '\t'), function (err) {
+   
+    fs.writeFile("README.md", "## Introduction"+ JSON.stringify(data, null, '\t'), function (err) {
 
         if (err) {
             return console.log(err);
